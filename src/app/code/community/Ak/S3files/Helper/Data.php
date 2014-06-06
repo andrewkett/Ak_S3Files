58d4c2aeedc07e909971dc8c94b684ca9e23d098
Andrew Kett <ajkett@gmail.com>
2014-06-06 10:57:51 +1000

    add log functions

--- src/app/code/community/Ak/S3files/Helper/Data.php
+++ src/app/code/community/Ak/S3files/Helper/Data.php
@@ -39,4 +39,16 @@ class Ak_S3files_Helper_Data extends Mage_Core_Helper_Abstract
     {
         return (bool) Mage::getStoreConfig(self::XML_PATH_S3_ENABLED);
     }
+
+
+
+    public function logImageEdit($message, $level = Zend_Log::DEBUG, $file = 'image_generation.log')
+    {
+        Mage::log($message, $level, $file);
+    }
+
+    public function logS3($message, $level = Zend_Log::DEBUG, $file = 's3_io.log')
+    {
+        Mage::log($message, $level, $file);
+    }
 }
